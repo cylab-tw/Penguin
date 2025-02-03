54bd8495ad161453f7df1311a1f3576f5862526d
jimnychen <jimnychen0826@gmail.com>
2025-02-03 09:55:01 +0800

    res.sendResponse("AR")

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { Server } from 'node-hl7-server'
 const server = new Server()
 
 const IB = server.createInbound({port: 3000}, async (req, res) => {
-    const messageReq = req.getMessage()._text
+    const messageReq = req.getMessage()
     const messageRes = res.getAckMessage()
-    console.log(messageReq)
+    var a = messageReq._text.split("|")
+    for(var i=0;i<a.length;i++){
+        console.log(a[i]+"\n")
+    }
+    await res.sendResponse("AR")
 })
